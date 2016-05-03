197c01648681cac3b9379c4c8564a805a9d870ad
Dan Allongo <daniel.s.allongo@gmail.com>
2016-05-02 22:42:28 -0600

    Initial commit
    Added GPL v2 license, readme, report descriptor.

---
+++ README.md
@@ -0,0 +1,20 @@
+# pySRD9c
+
+A Python test application for the Renovatio SRD-9c display
+
+This small application makes use of the pywinusb module (https://github.com/rene-aguirre/pywinusb) to interface with the SRD-9c display. The display uses he HID protocol to provide an input interface as a joystick and an output interface for the nine 7-segment displays and 16 LEDs. 
+
+The output report is structured as follows (41 bytes total):
+
+report id: 1 byte, always 0 as this is the only output report
+left display: 4 bytes, each bit is a single segment of the display in the standard order (4 digits)
+right display: 4 bytes, each bit is a single segment of the display in the standard order (4 digits)
+green/red RPM LEDs: 1 byte, the four low bits are the green LEDs, the four high bits are the red LEDs
+blue/status LEDs: 1 byte, the four low bits are the blue RPM LEDs, the four high bits are the status LEDS (low to high: red, yellow, blue, green)
+gear display: 1 byte, each bit is a single segment of the display in the standard order (1 digit)
+padding/unknown: 29 bytes, all 0 during normal operation, setting all bytes to 0xff resets the device
+
+### Releases
+#### 2016-05-02
+
+* Initial release
---
+++ pySRD9c.py
@@ -0,0 +1,207 @@
+"""
+pySRD9c.py - A Python test application for the Renovatio SRD-9c display
+by Dan Allongo (daniel.s.allongo@gmail.com)
+
+This small application makes use of the pywinusb module (https://github.com/rene-aguirre/pywinusb)
+to interface with the SRD-9c display. The display uses the HID protocol to provide an input interface
+as a joystick and an output interface for the nine 7-segment displays and 16 LEDs. 
+
+The output report is structured as follows (41 bytes total):
+report id: 1 byte, always 0 as this is the only output report
+left display: 4 bytes, each bit is a single segment of the display in the standard order (4 digits)
+right display: 4 bytes, each bit is a single segment of the display in the standard order (4 digits)
+green/red RPM LEDs: 1 byte, the four low bits are the green LEDs, the four high bits are the red LEDs
+blue/status LEDs: 1 byte, the four low bits are the blue RPM LEDs, the four high bits are the 
+	status LEDS (low to high: red, yellow, blue, green)
+gear display: 1 byte, each bit is a single segment of the display in the standard order (1 digit)
+padding/unknown: 29 bytes, all 0 during normal operation, setting all bytes to 0xff resets the device
+
+Release History:
+2016-05-02: Initial release
+"""
+
+from pywinusb import hid
+from time import sleep
+
+class srd9c:
+	lut = {
+	 '0':int('00111111', 2),
+	 '1':int('00000110', 2),
+	 '2':int('01011011', 2),
+	 '3':int('01001111', 2),
+	 '4':int('01100110', 2),
+	 '5':int('01101101', 2),
+	 '6':int('01111101', 2),
+	 '7':int('00000111', 2),
+	 '8':int('01111111', 2),
+	 '9':int('01101111', 2),
+	 'A':int('01110111', 2),
+	 'B':int('01111100', 2),
+	 'C':int('01011000', 2),
+	 'D':int('01011110', 2),
+	 'E':int('01111001', 2),
+	 'F':int('01110001', 2),
+	 'G':int('00111101', 2),
+	 'H':int('01110100', 2),
+	 'I':int('00010000', 2),
+	 'J':int('00001110', 2),
+	 'L':int('00111000', 2),
+	 'N':int('01010100', 2),
+	 'O':int('01011100', 2),
+	 'P':int('01110011', 2),
+	 'Q':int('01101011', 2),
+	 'R':int('01010000', 2),
+	 'S':int('01101101', 2),
+	 'T':int('01111000', 2),
+	 'U':int('00011100', 2),
+	 'Y':int('01101110', 2),
+	 'Z':int('01011011', 2),
+	 '-':int('01000000', 2),
+	 '_':int('00001000', 2),
+	 '=':int('01001000', 2),
+	 '"':int('00100010', 2),
+	 "'":int('00100000', 2),
+	 '`':int('00000010', 2),
+	 '.':int('10000000', 2)
+	}
+
+	def __init__(self, init_left='----', init_right='----', init_gear='-'):
+		self.device = None
+		self.output_report = None
+		self.left = init_left
+		self.right = init_right
+		self.gear = init_gear
+		self.rpm = {'green':[0]*4, 'red':[0]*4, 'blue':[0]*4, 
+			'use_green':True, 'use_red':True, 'use_blue':True, 'use_status':False,
+			'value':0}
+		self.status = [0]*4
+		while(not self.device):
+			devlist = hid.HidDeviceFilter(vendor_id = 0x04d8, product_id = 0xf667).get_devices()
+			if(devlist):
+				self.device = devlist[0]
+				self.device.open()
+				self.output_report = self.device.find_output_reports()[0]
+			else:
+				sleep(1)
+		self.send_report()
+		return
+
+	def pack_report(self):
+		while(len(self.left.replace('.', '')) > 4):
+			self.left = self.left[:-1]
+		while(len(self.left.replace('.', '')) < 4):
+			self.left = ' ' + self.left
+		while(len(self.right.replace('.', '')) > 4):
+			self.right = self.right[:-1]
+		while(len(self.right.replace('.', '')) < 4):
+			self.right = ' ' + self.right
+		while(len(self.gear.replace('.', '')) > 1):
+			self.gear = self.gear[:-1]
+		while(len(self.gear.replace('.', '')) < 1):
+			self.gear = ' ' + self.gear
+
+		left = []
+		right = []
+		gear = 0
+
+		for i in xrange(len(self.left)):
+			c = self.left[i].upper()
+			if c == '.':
+				continue
+			if c in self.lut:
+				c = self.lut[c]
+			else:
+				c = 0
+			if (i < len(self.left) - 1) and self.left[i + 1] == '.':
+				c += self.lut['.']
+			left.append(c)
+		for i in xrange(len(self.right)):
+			c = self.right[i].upper()
+			if c == '.':
+				continue
+			if c in self.lut:
+				c = self.lut[c]
+			else:
+				c = 0
+			if (i < len(self.right) - 1) and self.right[i + 1] == '.':
+				c += self.lut['.']
+			right.append(c)
+		for i in xrange(len(self.gear)):
+			c = self.gear[i].upper()
+			if c == '.':
+				continue
+			if c in self.lut:
+				c = self.lut[c]
+			else:
+				c = 0
+			if (i < len(self.gear) - 1) and self.gear[i + 1] == '.':
+				c += self.lut['.']
+			gear = c
+
+		rpm_on = 1
+		rpm_segments = 0
+		if(self.rpm['value'] < 0):
+			rpm_on = 0
+		if(self.rpm['use_green']):
+			rpm_segments += 1
+		if(self.rpm['use_red']):
+			rpm_segments += 1
+		if(self.rpm['use_blue']):
+			rpm_segments += 1
+		if(self.rpm['use_status']):
+			rpm_segments += 1
+		rpm = [rpm_on]*(int(rpm_segments*4*abs(self.rpm['value'])))
+		rpm += [(rpm_on^1)]*(rpm_segments*4 - len(rpm))
+		if(self.rpm['use_green']):
+			self.rpm['green'] = rpm[:4]
+			rpm = rpm[4:]
+		if(self.rpm['use_red']):
+			self.rpm['red'] = rpm[:4]
+			rpm = rpm[4:]
+		if(self.rpm['use_blue']):
+			self.rpm['blue'] = rpm[:4]
+			rpm = rpm[4:]
+		if(self.rpm['use_status']):
+			self.status = rpm[:4]
+
+		gr = 0
+		bs = 0
+
+		for i in xrange(4):
+			gr += self.rpm['green'][i] << i
+			gr += self.rpm['red'][i] << (i + 4)
+			bs += self.rpm['blue'][i] << i
+			bs += self.status[i] << (i + 4)
+
+		o = [0]
+		o += left
+		o += right
+		o += [gr, bs, gear]
+		o += [0]*(41 - len(o))
+		return o
+
+	def send_report(self):
+		self.output_report.send(self.pack_report())
+		return
+
+if __name__ == '__main__':
+	print "Waiting for device..."
+	test = srd9c(init_left='srd9', init_gear='c', init_right='init')
+	print "Device found!"
+	sleep(3)
+	print "Beginning test cycle (Ctrl-C to quit)..."
+	test.gear = 'n'
+	r = 0
+	rpm = 0
+	rpm_step = 0.01
+	test.rpm['use_status'] = True
+	while(True):
+		test.left = '{:.2f}'.format(r*-1)
+		test.right = '{:.2f}'.format(r)
+		if(abs(rpm) >= 1):
+			rpm_step *= -1
+		r -= rpm_step*3
+		rpm += rpm_step
+		test.rpm['value'] = rpm
+		test.send_report()
+		sleep(0.01)
